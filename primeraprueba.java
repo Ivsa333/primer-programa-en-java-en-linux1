7f832604948e82008206b22e716beb3a12c19726
Iván Borraz <ivanborrazgarcia@gmail.com>
2023-10-11 12:10:37 +0000

    v2

--- primeraprueba.java
+++ primeraprueba.java
@@ -3,6 +3,7 @@ public class primeraprueba {
 	public static void main(String args[]) {
 	
 		System.out.println("Mi deporte favorito es el baloncesto");
+		System.out.println("Mi lugar favorito es el parque");
 	}
 
 }
