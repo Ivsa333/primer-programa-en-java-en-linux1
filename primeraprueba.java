5111f904f1e0d15bdc41a781fe98d9af9b12c009
Iván Borraz <ivanborrazgarcia@gmail.com>
2023-10-11 12:12:45 +0000

    v3

--- primeraprueba.java
+++ primeraprueba.java
@@ -4,6 +4,7 @@ public class primeraprueba {
 	
 		System.out.println("Mi deporte favorito es el baloncesto");
 		System.out.println("Mi lugar favorito es el parque");
+		System.out.println("Mi red social favorita es Instagram");
 	}
 
 }
